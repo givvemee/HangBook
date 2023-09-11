af2473658751874eb3b7e21988998083ef4ceb02
givvemee <givvemeee@gmail.com>
2023-09-11 22:29:11 +0900

    chore: package 설치

--- next.config.js
+++ next.config.js
@@ -5,6 +5,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://www.aladin.co.kr/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
