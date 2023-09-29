4372d98a21e3098db2eb7e6bccce7a70bc74bfd2
Givvemee <givvemeee@gmail.com>
2023-09-29 20:47:04 +0900

    fix: 초기 스타일 렌더 문제 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,38 +4,34 @@ import Header from "@/components/header";
 import Mode from "@/components/mode";
 import Nav from "@/components/nav";
 import Quotes from "@/components/quotes";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useStore } from "store";
-import { StyleSheet } from "styles/StyleSheet";
 import "styles/reset.css";
 import { darkTheme } from "styles/stitches.config";
 import { FullWrap, LeftSide, MainBack, RightSide } from "./style";
+import { ServerStylesheet } from "styles/StyleSheet";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   const { isDarkMode } = useStore();
-  const [isReady, setIsReady] = useState(false);
-
-  // useEffect(() => {
-  //   setIsReady(true);
-  // }, []);
 
   return (
     <html lang="ko">
-      <StyleSheet />
       <body>
-        <div className={!isDarkMode ? darkTheme : ""}>
-          <MainBack>
-            <FullWrap>
-              <LeftSide>
-                <Header />
-                <Nav />
-                <Quotes />
-              </LeftSide>
-              <RightSide>{children}</RightSide>
-            </FullWrap>
-            <Mode />
-          </MainBack>
-        </div>
+        <ServerStylesheet>
+          <div className={!isDarkMode ? darkTheme : ""}>
+            <MainBack>
+              <FullWrap>
+                <LeftSide>
+                  <Header />
+                  <Nav />
+                  <Quotes />
+                </LeftSide>
+                <RightSide>{children}</RightSide>
+              </FullWrap>
+              <Mode />
+            </MainBack>
+          </div>
+        </ServerStylesheet>
       </body>
     </html>
   );
--- styles/StyleSheet.tsx
+++ styles/StyleSheet.tsx
@@ -1,7 +1,15 @@
-import { getCssText } from './stitches.config';
+"use client";
 
-export const StyleSheet = () => {
-  return (
-    <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
-  );
-};
+import React, { ReactNode } from "react";
+import { useServerInsertedHTML } from "next/navigation";
+import { getCssText } from "./stitches.config";
+
+export function ServerStylesheet({ children }: { children: ReactNode }) {
+  useServerInsertedHTML(() => {
+    return (
+      <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+    );
+  });
+
+  return children;
+}
